20fd704f210f1323fb1be93a101e9c1835c76545
Conner Fritz <me@connerfritz.com>
2021-07-09 19:27:46 -0400

    Updates

---
+++ precache-manifest.0a63f05e16b2f6d494bb2cdf9b45109b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b77fe35f906d2c6d7c06f599f318d5af",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3a1b053c7b61adddd848",
+    "url": "./static/css/main.aa195e40.chunk.css"
+  },
+  {
+    "revision": "82b3e64a2c08db0ab390",
+    "url": "./static/js/2.5a64f740.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.5a64f740.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a1b053c7b61adddd848",
+    "url": "./static/js/main.758a97e3.chunk.js"
+  },
+  {
+    "revision": "a29a82a13aeba05d7664",
+    "url": "./static/js/runtime-main.80fc59fa.js"
+  }
+]);
